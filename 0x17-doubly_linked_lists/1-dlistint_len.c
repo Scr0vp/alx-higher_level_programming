c8e4b34a90127af6a0ed028f2b49fc3669c433bd
root <hmostaghfir@gmail.com>
2024-01-11 18:03:30 -0800

    update fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,23 +9,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    /*Variable to store the number of elements in the list.*/
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-
-    while (h)
-    {
-        /*Increment the element count.*/
-        nodes++;
-
-
-
-
-        /*Move to the next element.*/
-        h = h->next;
-    }
-
-
-    /*Return the total number of elements in the list.*/
-    return (nodes);
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
 }
