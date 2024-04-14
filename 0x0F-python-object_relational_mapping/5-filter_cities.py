47eb581aea71e3ccb69c89fec655657934026b1d
root <hmostaghfir@gmail.com>
2024-04-14 16:34:37 -0700

    Renamed row

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
-"""5-filter_cities
-Takes in the name of a state as an argument and lists all cities of that state,
-using the database hbtn_0e_4_usa
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
 """
 
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+import MySQLdb as db
+from sys import argv
 
-    with MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                         db=argv[3], port=3306) as db:
-        db.execute("SELECT cities.name\
-                    FROM cities\
-                    LEFT JOIN states\
-                    ON cities.state_id=states.id\
-                    WHERE states.name = %s\
-                    ORDER by cities.id ASC", (argv[4],))
-        table = db.fetchall()
-        print(", ".join([data[0] for data in table]))
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
