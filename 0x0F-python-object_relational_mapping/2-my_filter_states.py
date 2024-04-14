44c63989d615cd09dab9524f67c72ef437afbc49
root <hmostaghfir@gmail.com>
2024-04-14 16:10:08 -0700

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-"""2-my_filter_states.py module
-Takes in an argument and displays all values in the states table of
-hbtn_0e_0_usa where name matches the argument
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
 
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+import MySQLdb as db
+from sys import argv
 
-    with MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                         db=argv[3], port=3306) as db:
-        db.execute("SELECT *\
-                   FROM states\
-                   WHERE name\
-                   LIKE BINARY '{}'\
-                   ORDER BY id ASC".format(argv[4]))
-        table = db.fetchall()
-        for data in table:
-            print(data)
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+                        states.id ASC".format(argv[4]))
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
